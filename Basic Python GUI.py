799e7caa718ff92fb0c1804c63898ab8635383f3
DevYouriD <youri.dera@gmail.com>
2022-08-29 18:08:33 +0200

    Add files via upload

---
+++ Basic Python GUI.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+window = Tk()
+#window.iconbitmap('Resources/[FILENAME].ico')
+window.title('Password Generator GUI')
+window.config(height=500, width=500, background='black')
+
+#headerImage = PhotoImage(file='Resources/[FILENAME].png')
+#Label(window, image=headerImage, bg='black').place(relx=0.5, rely=0.16, anchor=CENTER)
+
+#MAIN FUNCTIONALITY
+
+#CLOSE
+def close_window():
+    window.destroy()
+    exit()
+
+Button(window, text='EXIT', width=6, command=close_window, font='none 10 bold').place(relx=0.08, rely=0.95, anchor=CENTER)
+
+window.mainloop()
